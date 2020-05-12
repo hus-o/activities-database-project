90d0b5cb2c37486a4441977d4543313978424127
amita125 <amitaghale@gmail.com>
2020-05-12 17:48:03 +0100

    database data displayed from index file

---
+++ knex/migrations/20200512171832_activities_table.js
@@ -0,0 +1,12 @@
+exports.up = function (knex) {
+  return knex.schema.createTable("activities", (table) => {
+    table.increments("id").primary();
+    table.string("name");
+    table.string("location", 256);
+    table.integer("activity_time_minutes");
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists("activities");
+};
--- knex/seeds/activity_seed.js
+++ knex/seeds/activity_seed.js
@@ -1,28 +1,28 @@
-
-exports.seed = function(knex) {
+exports.seed = function (knex) {
   // Deletes ALL existing entries
-  return knex('activities').del()
+  return knex("activities")
+    .del()
     .then(function () {
       // Inserts seed entries
-      return knex('activities').insert([
+      return knex("activities").insert([
         {
           id: 1,
-          name: 'Ice Skating',
+          name: "Ice Skating",
           location: "Alexandra Palace",
-          activity_time_minutes: 60
+          activity_time_minutes: 60,
         },
         {
-          id: 2, 
-          name: 'Badminton',
+          id: 2,
+          name: "Badminton",
           location: "Leisure Centre",
-          activity_time_minutes: 60
+          activity_time_minutes: 60,
         },
         {
-          id: 3, 
-          name: 'Go To Science Museum',
+          id: 3,
+          name: "Go To Science Museum",
           location: "Science Museum",
-          activity_time_minutes: 150
-        }
+          activity_time_minutes: 150,
+        },
       ]);
     });
 };
