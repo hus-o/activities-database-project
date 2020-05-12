c97c0b535ade15a56e25abe06bfc4e5c97ebeacd
huseyin <thehusko@gmail.com>
2020-05-12 15:53:32 +0100

    added pug and dotenv

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
+require('dotenv').config()
 const express = require('express');
 const PORT = process.env.PORT;
+const path = require("path")
 const knex = require('./knex/knex.js');
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
 app.get('/', (req, res) => {
-    res.send("hi")
-  // use the knex variable above to create dynamic queries
+    res.render("index")
 });
 
 app.listen(PORT, () => {
