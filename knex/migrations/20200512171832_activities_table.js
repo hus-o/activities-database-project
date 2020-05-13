9a01c2a7b1dde34bb1827c96e668581fcb00a4e9
amita125 <amitaghale@gmail.com>
2020-05-13 11:03:12 +0100

    front-end layout

--- knex/migrations/20200512171832_activities_table.js
+++ knex/migrations/20200512171832_activities_table.js
@@ -1,9 +1,10 @@
 exports.up = function (knex) {
   return knex.schema.createTable("activities", (table) => {
     table.increments("id").primary();
-    table.string("name");
-    table.string("location", 256);
-    table.integer("activity_time_minutes");
+    table.string("name", 256).notNullable();
+    table.string("location", 128).notNullable();
+    table.string("type", 128).notNullable();
+    table.integer("activity_time_minutes").notNullable();
   });
 };
 
--- server.js
+++ server.js
@@ -1,18 +1,19 @@
-require('dotenv').config()
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const PORT = process.env.PORT;
-const path = require("path")
-const activities = require("./routes/activities")
-const knex = require('./knex/knex.js');
+const path = require("path");
+const activities = require("./routes/activities");
+const knex = require("./knex/knex.js");
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.set('view engine', 'pug');
-app.set('views', path.join(__dirname, 'views'));
+app.engine("pug", require("pug").__express);
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
 
-app.use("/", activities)
+app.use("/", activities);
 
 /* app.get('/', (req, res) => {
     res.render("index")
