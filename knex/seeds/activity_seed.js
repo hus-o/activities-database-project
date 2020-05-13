d1ff1794690b1b07d35f929f918c935ee15b6ba6
huseyin <thehusko@gmail.com>
2020-05-13 15:39:11 +0100

    updated seed and view

--- knex/seeds/activity_seed.js
+++ knex/seeds/activity_seed.js
@@ -6,28 +6,24 @@ exports.seed = function(knex) {
       // Inserts seed entries
       return knex('activities').insert([
         {
-          id: 1,
           name: 'Ice Skating',
           location: " N22 7AY",
           type: "Sport",
           activity_time_minutes: 60
         },
         {
-          id: 2, 
           name: 'Badminton',
           location: "Any Leisure Centre",
           type: "Sport",
           activity_time_minutes: 60
         },
-        {
-          id: 3, 
+        { 
           name: 'Science Museum',
           location: "SW7 2DD",
           type: "Museum",
           activity_time_minutes: 150
         },
         {
-          id: 4, 
           name: 'Flat Iron',
           location: "WC2E 8QH",
           type: "Food",
